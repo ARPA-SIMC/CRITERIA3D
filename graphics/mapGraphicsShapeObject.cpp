2a7cb9edbdfbfcf245a3e9fc17aaa3b93f40ac4d
ftomei <ftomei@arpae.it>
2020-01-28 20:18:07 +0100

    fix zonal statistic

--- graphics/mapGraphicsShapeObject.cpp
+++ graphics/mapGraphicsShapeObject.cpp
@@ -102,7 +102,7 @@ void MapGraphicsShapeObject::drawShape(QPainter* myPainter)
     QPainterPath* path;
     QPainterPath* inner;
 
-    myPainter->setPen(Qt::black);
+    myPainter->setPen(Qt::gray);
     myPainter->setBrush(Qt::NoBrush);
 
     for (unsigned long i = 0; i < nrShapes; i++)
