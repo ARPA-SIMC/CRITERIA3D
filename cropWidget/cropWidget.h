582bc676d0691610b3761ae107bcefbd058d23f2
lauracosta <lauramail@iol.it>
2020-02-07 10:28:42 +0100

    add cropWidget file

---
+++ cropWidget/cropWidget.h
@@ -0,0 +1,14 @@
+#ifndef CROPWIDGET_H
+#define CROPWIDGET_H
+
+#include <QWidget>
+
+class Crit3DCropWidget : public QWidget
+{
+    Q_OBJECT
+
+    public:
+        Crit3DCropWidget();
+};
+
+#endif // CROPWIDGET_H
