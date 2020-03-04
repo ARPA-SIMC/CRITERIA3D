a50a177ce85cfeddb598b3ed1bf7ad7e5220f057
Fausto Tomei <ftomei@arpae.it>
2020-03-04 18:14:19 +0100

    update rootdensity

--- cropWidget/tabRootDensity.cpp
+++ cropWidget/tabRootDensity.cpp
@@ -38,7 +38,7 @@ TabRootDensity::TabRootDensity()
     chart->addAxis(axisX, Qt::AlignBottom);
     seriesRootDensity->attachAxis(axisX);
 
-    axisY->setTitleText("Depth  [m]");
+    axisY->setTitleText("Depth [m]");
     //categories << "2.0" << "1.8" << "1.6" << "1.4" << "1.2" << "1.0" << "0.8" << "0.6" << "0.4" << "0.2" << "0.0";
     categories << "2.00" << "1.80" << "1.60" << "1.40" << "1.20" << "1.00" << "0.80" << "0.60" << "0.40" << "0.20" << "0.00";
     axisY->append(categories);
@@ -80,15 +80,15 @@ void TabRootDensity::computeRootDensity(Crit3DCrop* myCrop, Crit3DMeteoPoint *me
     {
         if (soilLayers[i].depth <= 2)
         {
-            if (!categories.contains(QString::number(soilLayers[i].depth, 'f', 2)))
+            double layerDepth = soilLayers[i].depth + soilLayers[i].thickness/2;
+            QString depthStr = QString::number(layerDepth, 'f', 1);
+            if (!categories.contains(depthStr))
             {
-                categories << QString::number(soilLayers[i].depth, 'f', 2);
+                categories << depthStr;
             }
-
         }
     }
-    qDebug() << "categories " << categories;
-    qDebug() << "categories size " << categories.size();
+
     axisY->append(categories);
     int currentDoy = 1;
     myCrop->initialize(meteoPoint->latitude, nrLayers, totalSoilDepth, currentDoy);
@@ -131,7 +131,7 @@ void TabRootDensity::updateRootDensity()
     double waterTableDepth = NODATA;
     double tmin;
     double tmax;
-    double maxDepth = 0;
+    double maxRootDensity = 0;
     for (Crit3DDate myDate = firstDate; myDate <= lastDate; ++myDate)
     {
         tmin = mp->getMeteoPointValueD(myDate, dailyAirTemperatureMin);
@@ -151,18 +151,17 @@ void TabRootDensity::updateRootDensity()
                 if (layers[i].depth <= 2)
                 {
                     *set << crop->roots.rootDensity[i]*100;
-                    if (crop->roots.rootDensity[i]*100 > maxDepth)
+                    if (crop->roots.rootDensity[i]*100 > maxRootDensity)
                     {
-                        maxDepth = crop->roots.rootDensity[i]*100;
+                        maxRootDensity = crop->roots.rootDensity[i]*100;
                     }
                 }
-
             }
-
         }
     }
-    maxDepth = qRound(maxDepth);
-    axisX->setRange(0,maxDepth);
+
+    maxRootDensity = ceil(maxRootDensity);
+    axisX->setRange(0, maxRootDensity);
     seriesRootDensity->append(set);
     chart->addSeries(seriesRootDensity);
 }
