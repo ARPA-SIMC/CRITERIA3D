b990ff30dbebbeae426ef0e9256df86971e72074
lauracosta <lauramail@iol.it>
2020-05-20 20:54:02 +0200

    clean gridCellMarker

--- graphics/stationMarker.cpp
+++ graphics/stationMarker.cpp
@@ -117,35 +117,6 @@ void StationMarker::setToolTip()
     CircleObject::setToolTip(toolTipText);
 }
 
-/*
-QString StationMarker::getToolTipText()
-{
-    QString idpoint = QString::fromStdString(_id);
-    QString name = QString::fromStdString(_name);
-    QString dataset = QString::fromStdString(_dataset);
-    double altitude = _altitude;
-    QString municipality = QString::fromStdString(_municipality);
-
-    QString toolTipText = QString("<b> %1 </b> <br/> ID: %2 <br/> dataset: %3 <br/> altitude: %4 m <br/> municipality: %5")
-                            .arg(name).arg(idpoint).arg(dataset).arg(altitude).arg(municipality);
-
-    if (_currentValue != NODATA)
-    {
-        QString value = QString::number(double(_currentValue));
-
-        QString myQuality = "";
-        if (_quality == quality::wrong_syntactic)
-            myQuality = "WRONG DATA (syntax control)";
-        if (_quality == quality::wrong_spatial)
-            myQuality = "WRONG DATA (spatial control)";
-
-        toolTipText = QString("value: <b> %1 <br/> %2 <br/> </b>").arg(value).arg(myQuality) + toolTipText;
-    }
-    return toolTipText;
-}
-*/
-
-
 void StationMarker::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
 {  
     if (event->button() == Qt::RightButton)
