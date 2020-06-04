5deabcc3e0e25424e02a808d66051acd0af427ad
voltAntonio <avolta@arpa.emr.it>
2020-06-04 17:43:39 +0200

    weather generator prova C4

--- weatherGenerator2D/wg2D_temperature.cpp
+++ weatherGenerator2D/wg2D_temperature.cpp
@@ -1202,7 +1202,7 @@ void weatherGenerator2D::multisiteTemperatureGeneration()
         {
 
 
-            printf("%.1f %.1f\n",residuals[0],residuals[1]);
+            //printf("%.1f %.1f\n",residuals[0],residuals[1]);
             residuals[0] = temperatureCoefficients[i].A[0][0]*residuals[0] + temperatureCoefficients[i].A[0][1]*residuals[1];
             residuals[0] += temperatureCoefficients[i].B[0][0]*eps[0][j] + temperatureCoefficients[i].B[0][1]*eps[1][j];
             residuals[1] = temperatureCoefficients[i].A[1][0]*residuals[0] + temperatureCoefficients[i].A[1][1]*residuals[1];
