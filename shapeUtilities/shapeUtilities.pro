4b26bee2d76dcbdbbaba205bde83967e24338c57
lauracosta <lauramail@iol.it>
2020-06-24 21:59:39 +0200

    UCM CSV lowercase

--- shapeUtilities/shapeUtilities.pro
+++ shapeUtilities/shapeUtilities.pro
@@ -28,7 +28,7 @@ DEFINES += _CRT_SECURE_NO_WARNINGS
 INCLUDEPATH =  ../crit3dDate ../mathFunctions ../gis ../shapeHandler ../project
 
 SOURCES += \
-    shapeFromCSVForShell.cpp \
+    shapeFromCsvForShell.cpp \
     shapeToRaster.cpp    \
     shapeUtilities.cpp   \
     ucmUtilities.cpp     \
@@ -39,7 +39,7 @@ SOURCES += \
 
 
 HEADERS += \
-    shapeFromCSVForShell.h \
+    shapeFromCsvForShell.h \
     shapeToRaster.h    \
     shapeUtilities.h   \
     ucmUtilities.h     \
--- shapeUtilities/ucmUtilities.h
+++ shapeUtilities/ucmUtilities.h
@@ -7,6 +7,6 @@
     #include <QString>
 
     bool writeUcmListToDb(Crit3DShapeHandler* shapeHandler, QString dbName, std::string *error);
-    bool shapeFromCSV(Crit3DShapeHandler* shapeHandler, Crit3DShapeHandler* outputShape, QString fileCSV, QString fileCSVRef, QString outputName, std::string *error, bool showInfo);
+    bool shapeFromCsv(Crit3DShapeHandler* shapeHandler, Crit3DShapeHandler* outputShape, QString fileCsv, QString fileCsvRef, QString outputName, std::string *error, bool showInfo);
 
 #endif // UCMUTILITIES_H
