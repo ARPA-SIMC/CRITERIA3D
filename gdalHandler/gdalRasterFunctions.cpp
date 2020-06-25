31a5593017ce76a737f2ada9e11120e4f8205a8b
ftomei <ftomei@arpae.it>
2020-06-25 17:09:14 +0200

    dataset

--- gdalHandler/gdalRasterFunctions.cpp
+++ gdalHandler/gdalRasterFunctions.cpp
@@ -2,6 +2,7 @@
 #include "gdalRasterFunctions.h"
 #include "commonConstants.h"
 
+#include <iostream>
 #include <gdal_priv.h>
 
 #include <QString>
@@ -20,19 +21,33 @@ bool readGdalRaster(QString fileName, gis::Crit3DRasterGrid* myRaster, QString* 
         return false;
     }
 
+    // Driver
+    double adfGeoTransform[6];
+    qDebug() << "Driver: " << dataset->GetDriver()->GetDescription() << dataset->GetDriver()->GetMetadataItem(GDAL_DMD_LONGNAME);
+
+    // size
+    qDebug() << "Size = " << dataset->GetRasterXSize() << dataset->GetRasterYSize() << dataset->GetRasterCount();
+
+    // projection
+    if (dataset->GetProjectionRef() != nullptr)
+        qDebug() << "Projection = " << dataset->GetProjectionRef();
+
     // TODO chek spatial reference
     OGRSpatialReference spatialReference;
     spatialReference.SetWellKnownGeogCS("WGS84");
-    const char* projRef = dataset->GetProjectionRef();
-    qDebug(projRef);
+    // TODO check utm zone
 
-    // TODO read utm zone
-
-    int nBands = dataset->GetRasterCount();
-    qDebug() << "Nr. band: " << QString::number(nBands);
+    // Origin and size
+    if (dataset->GetGeoTransform(adfGeoTransform) == CE_None)
+    {
+        qDebug() << "Origin = " << adfGeoTransform[0] << adfGeoTransform[3];
+        qDebug() << "Pixel Size = " << adfGeoTransform[1] << adfGeoTransform[5];
+    }
 
     // TODO select band
+    qDebug() << "Nr. band: " << dataset->GetRasterCount();
     GDALRasterBand* band = dataset->GetRasterBand(1);
+
     if(band == nullptr)
     {
         *myError = "Band 1 is void!";
