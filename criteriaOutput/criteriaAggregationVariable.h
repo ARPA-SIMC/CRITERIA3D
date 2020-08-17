6c2448917a93417e45398276a04eefc9b521dd9d
lauracosta <lauramail@iol.it>
2020-08-17 13:07:57 +0200

    cirteriaAggregationVariable

---
+++ criteriaOutput/criteriaAggregationVariable.h
@@ -0,0 +1,20 @@
+#ifndef CRITERIAAGGREGATIONVARIABLE_H
+#define CRITERIAAGGREGATIONVARIABLE_H
+
+    #include <QString>
+    #include <QStringList>
+
+    #define CSVREQUIREDINFO 3
+
+    class CriteriaAggregationVariable
+    {
+    public:
+        QStringList outputVarName;
+        QStringList inputField;
+        QStringList computation;
+
+        CriteriaAggregationVariable();
+        bool parserAggregationVariable(QString fileName, QString &error);
+    };
+
+#endif // CRITERIAAGGREGATIONVARIABLE_H
