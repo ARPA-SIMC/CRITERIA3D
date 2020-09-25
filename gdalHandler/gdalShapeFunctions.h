ef661eb38081ac12330f75e7f02319543aad94ea
lauracosta <lauramail@iol.it>
2020-09-25 09:34:21 +0200

    gdalShapeIntersection

--- gdalHandler/gdalShapeFunctions.h
+++ gdalHandler/gdalShapeFunctions.h
@@ -34,18 +34,6 @@ static QMap<QString, QString> mapExtensionShortName
     {"bil", "EHdr"}
 };
 
-bool computeUcmIntersection(Crit3DShapeHandler *ucm, Crit3DShapeHandler *crop, Crit3DShapeHandler *soil, Crit3DShapeHandler *meteo,
-                 std::string idCrop, std::string idSoil, std::string idMeteo, QString ucmFileName, std::string *error);
-
-
-bool shapeIntersection(Crit3DShapeHandler *first, Crit3DShapeHandler *second, GEOSGeometry **inteserctionGeom);
-bool getShapeFromGeom(GEOSGeometry *inteserctionGeom, Crit3DShapeHandler *ucm);
-GEOSGeometry *loadShapeAsPolygon(Crit3DShapeHandler *shapeHandler);
-OGRGeometry* loadShapeAsPolygon(Crit3DShapeHandler *shapeHandler, QString &errorStr);
 bool shapeToRaster(QString shapeFileName, std::string shapeField, QString resolution, QString proj, QString outputName, QString &errorStr);
-
-//GEOSGeometry * SHPObject_to_GeosPolygon_NoHoles(SHPObject *object);
-//GEOSGeometry *load_shapefile_as_collection(char *pathname);
-//GEOSGeometry * testIntersection();
 
 #endif // GDALSHAPEFUNCTIONS_H
