c706b6fa4afe0da98e5b73a01e51a29c3b797a7d
lauracosta <lauramail@iol.it>
2021-09-21 18:15:10 +0200

    add set/get functions

--- drought/drought.cpp
+++ drought/drought.cpp
@@ -4,3 +4,73 @@ Drought::Drought()
 {
 
 }
+
+meteoVariable Drought::getVar() const
+{
+    return var;
+}
+
+void Drought::setVar(const meteoVariable &value)
+{
+    var = value;
+}
+
+droughtIndex Drought::getIndex() const
+{
+    return index;
+}
+
+void Drought::setIndex(const droughtIndex &value)
+{
+    index = value;
+}
+
+int Drought::getTimeScale() const
+{
+    return timeScale;
+}
+
+void Drought::setTimeScale(int value)
+{
+    timeScale = value;
+}
+
+int Drought::getFirstYear() const
+{
+    return firstYear;
+}
+
+void Drought::setFirstYear(int value)
+{
+    firstYear = value;
+}
+
+int Drought::getLastYear() const
+{
+    return lastYear;
+}
+
+void Drought::setLastYear(int value)
+{
+    lastYear = value;
+}
+
+bool Drought::getUseClima() const
+{
+    return useClima;
+}
+
+void Drought::setUseClima(bool value)
+{
+    useClima = value;
+}
+
+bool Drought::getComputeAll() const
+{
+    return computeAll;
+}
+
+void Drought::setComputeAll(bool value)
+{
+    computeAll = value;
+}
--- drought/drought.h
+++ drought/drought.h
@@ -12,12 +12,36 @@ class Drought
 public:
     Drought();
 
+    meteoVariable getVar() const;
+    void setVar(const meteoVariable &value);
+
+    droughtIndex getIndex() const;
+    void setIndex(const droughtIndex &value);
+
+    int getTimeScale() const;
+    void setTimeScale(int value);
+
+    int getFirstYear() const;
+    void setFirstYear(int value);
+
+    int getLastYear() const;
+    void setLastYear(int value);
+
+    bool getUseClima() const;
+    void setUseClima(bool value);
+
+    bool getComputeAll() const;
+    void setComputeAll(bool value);
+
 private:
-    Crit3DMeteoPoint meteoPoint;
+    Crit3DMeteoPoint* meteoPoint;
+    meteoVariable var;
     droughtIndex index;
     int timeScale;
     int firstYear;
     int lastYear;
+    bool useClima;
+    bool computeAll;
 
 };
 
