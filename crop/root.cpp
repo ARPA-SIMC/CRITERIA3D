04ae66d1044f0c4a0914000023a4a667f2b9bdd8
voltAntonio <avolta@arpa.emr.it>
2021-10-07 14:56:17 +0200

    aggiustamento funzione gamma per valori estremi

--- crop/root.cpp
+++ crop/root.cpp
@@ -485,15 +485,13 @@ namespace root
         }
         else if (myCrop->roots.rootShape == GAMMA_DISTRIBUTION)
         {
-            double normalizationFactor ;
+            //double normalizationFactor ;
             double kappa, theta,a,b;
             double mean, mode;
             mean = myCrop->roots.rootLength * 0.5;
             mode = myCrop->roots.rootLength * 0.2;
             theta = mean - mode;
             kappa = mean / theta;
-            // complete gamma function
-            normalizationFactor = Gamma_Function(kappa);
 
             for (i=1 ; i < nrLayers; i++)
             {
@@ -501,8 +499,7 @@ namespace root
                 if (b>0)
                 {
                     a = MAXVALUE(soilLayers[i].depth - soilLayers[i].thickness*0.5 - myCrop->roots.rootDepthMin,0); // left extreme
-                    myCrop->roots.rootDensity[i] = Incomplete_Gamma_Function(b/theta,kappa) - Incomplete_Gamma_Function(a/theta,kappa);
-                    myCrop->roots.rootDensity[i] /= normalizationFactor;
+                    myCrop->roots.rootDensity[i] = incompleteGamma(kappa,b/theta) - incompleteGamma(kappa,a/theta); // incompleteGamma is already normalized by gamma(kappa)
                 }
             }
         }
