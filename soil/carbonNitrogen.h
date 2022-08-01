55d28550a383a17afad0ce50df4ae26f590eba29
voltAntonio <avolta@arpa.emr.it>
2022-08-01 16:55:52 +0200

    salvataggio traduzione fino a linea 1700

--- soil/carbonNitrogen.h
+++ soil/carbonNitrogen.h
@@ -212,6 +212,212 @@ class Crit3DCarbonNitrogenWholeProfile
 #endif // CARBON_H
 /*
  *
+void soluteFluxesPiston_old(float* mySolute, float* leached, float* CoeffPiston)
+// 2008.10 FT GA
+// calcolo dei flussi di nitrati gravitazionali (a 'pistone')
+
+    int L;
+    int minFluxPiston = 5;
+
+    ReDim u_media(nrLayers) As Double
+    ReDim F_Solute(nrLayers) As Double
+    ReDim Solute_Macro(nrLayers) As Double
+    ReDim CoeffPiston(nrLayers)
+
+    //'initialize
+    for (L = 0;L<nrLayers; L++)
+    {
+        u_media[L] = (U[L] + umid[L].BeforeInfiltration) / 2;
+        //'u_media(L) = U(L)
+        if (u_media[L] > suolo[L].CC)
+            Solute_Macro[L] = (mySolute[L] / u_media[L]) * (u_media[L] - suolo[L].CC);
+        else
+            Solute_Macro[L] = 0;
+    }
+
+    f_Solute[0] = 0;
+
+    for (L = 1; L< nrLayers; L++)
+    {
+        F_Solute[L] = 0;
+        if (u_media[L] <= suolo[L].CC || Flux(L) <= 0)
+            CoeffPiston[L] = 0;
+        else
+            CoeffPiston[L] = minValue(1, Flux[L] / (u_media[L] - suolo[L].CC)) * minValue(1, Flux[L] / minFluxPiston);
+
+        //'azoto in entrata da nodo L-1
+        Solute_Macro[L] += F_Solute[L - 1];
+        mySolute[L] += F_Solute[L - 1];
+
+        //'calcolo flussi convettivi
+        if (CoeffPiston[L] > 0)
+        {
+            f_Solute[L] = Solute_Macro[L] * CoeffPiston[L];
+
+            //'azoto in uscita da nodo L
+            Solute_Macro[L] -= F_Solute[L];
+            mySolute[L] -= F_Solute[L];
+        }
+    }
+
+    //leaching
+    *leached += F_Solute[nrLayers-1];
+
+}
+
+
+void soluteFluxes(float* mySolute(),bool flagRisalita, float pistonDepth,float* )
+
+    //2008.10 GA eliminata parte dispersiva perché il meccanismo pseudo-numerico è già dispersivo di suo
+    //2008.09 GA inserita componente dispersiva
+    //2008.03 GA FT inserita componente diffusiva
+    //2007.04 FT GA sistemato algoritmo pseudo-numerico a iterazione
+    //04.03.02.FZ
+    //-------------- NOTE -----------------------------------------------------
+    //calcolo dei flussi di soluti con diluizione iterativa
+
+    int L;                          //[-] contatore
+    float* flux_Solute();           //[g m-2] flussi soluto
+    int i;                          //[-] contatore
+    int iterations;       //[-] numero di iterazioni per la diluizione
+    double* f_Solute;
+    double H2O_step_flux;
+    double H2O_step_flux_L_1;
+    double U_vol;
+    int firstLayer;
+    float myFreeSolute;
+    float coeffMobile;
+
+        if (pistonDepth >= suolo[nrLayers].prof)
+            return;
+        else
+        {
+            for (L=1; L<; nrLayers;L++)
+
+                If suolo(L).prof >= PistonDepth Then Exit For
+            Next L
+            FirstLayer = L
+            L=0;
+            while(suolo[L].prof >= pistonDepth)
+            {
+                L++;
+            }
+            firstLayer = L;
+            L=0;
+        }
+        flux_solute = (float *) calloc(nrLayers, sizeof(float));
+        double *u_temp = (double *) calloc(nrLayers, sizeof(double));
+        double *f_solute = (double *) calloc(nrLayers, sizeof(double));
+
+        for (L = 0; L<nrLayers; L++)
+        {
+            flux_Solute[L] = 0;
+            u_temp[L] = umid[L].BeforeInfiltration;
+        }
+        // ???????????????????
+        For L = nrLayers To 1 Step -1
+
+        Next L
+
+        f_Solute[0] = 0;
+        // ??????????????????????????
+
+        //iterazioni = min(max(24, 0.1 * max(Flux(0), Abs(Flux(nrLayers))) * max(Flux(0), Abs(Flux(nrLayers)))), 1000)
+
+        iterations = 1;
+        for (i = 0; i<iterations; i++)
+        {
+            For (L = firstLayer; L<nrLayers; L++)
+            {
+                f_Solute[L] = 0;
+
+                H2O_step_flux = (Flux[L] / iterations);
+                H2O_step_flux_L_1 = (Flux[L - 1] / iterations);
+
+                // acqua in entrata/uscita da nodo L
+                u_temp[L] += H2O_step_flux_L_1 - H2O_step_flux;
+
+                // calcolo flussi soluto
+                if (Flux[L] > 0)
+                {
+                    CoeffMobile = 1;
+                    myFreeSolute = mySolute[L] * CoeffMobile;
+                    f_Solute[L] = minValue(mySolute[L], myFreeSolute / umid[L].BeforeInfiltration * H2O_step_flux);
+                }
+                else if (flagRisalita && (Flux[L] < 0) && (L < nrLayers))
+                {
+                    //myFreeSolute = mySolute[L + 1] * CoeffMobile;
+                    myFreeSolute = mySolute[L + 1];
+                    f_Solute[L] = min(mySolute[L + 1], myFreeSolute / umid[L + 1].BeforeInfiltration * H2O_step_flux)
+                }
+
+                //azoto in entrata/uscita da nodo L-1
+                mySolute[L] += f_Solute[L - 1] - f_Solute[L];
+
+                //flussi convettivi totali
+                flux_Solute[L] += f_Solute[L];
+
+            }
+        }
+
+        // leaching
+        // FT GA 2007.12
+        *leached += Flux_Solute[nrLayers-1];
+
+}
+
+
+
+
+// function develpoed by V. Marletto for watertable
+void leachingWaterTable(float* mySolute, float* leached)
+{
+    int L;
+    double mySolute_leach_edge;
+
+    // dilavamento
+    if ((waterTable != NODATA) && (waterTable > 0) && (flagWaterTable == 1) && (flagWaterTableCase == 1))
+    {
+        for (L = 0; L< Layers; L++)
+        {
+            if (suolo[L].prof > waterTable)
+            {
+                leached += mySolute[L]
+                mySolute[L] = 0;
+            }
+            else if (suolo[L].prof >= waterTable - MAX_FRANGIA_CAPILLARE)
+            {
+                mySolute_leach_edge = (mySolute[L] / MAX_FRANGIA_CAPILLARE) * (MAX_FRANGIA_CAPILLARE - (waterTable - suolo[L].prof))
+                mySolute[L] += - mySolute_leach_edge;
+                leached += mySolute_leach_edge;
+            }
+        }
+    }
+
+}
+
+void NH4_Balance()
+{
+    float profileNH4PreviousDay;
+
+    profileNH4PreviousDay = profileNH4;
+    // ProfiloNH4 = ProfileSum(N_NH4())
+    profileNH4 = 0;
+    for (int i=0;i<nrLayers;i++)
+    {
+        profileNH4 += N_NH4[i];
+    }
+
+    balanceFinalNH4 = profileNH4 - profileNH4PreviousDay - N_NH4_fertGG + N_imm_l_NH4GG;
+    balanceFinalNH4 += - N_min_humusGG - N_min_litterGG;
+    balanceFinalNH4 += N_NH4_volGG - N_Urea_HydrGG + N_nitrifGG;
+    balanceFinalNH4 += N_NH4_uptakeGG;
+    balanceFinalNH4 += N_NH4_runoff0GG + N_NH4_runoffGG + Flux_NH4GG - PrecN_NH4GG;
+
+    //If BilFinaleNH4 > 0.01 Then Stop
+    return;
+}
+
 void NO3_Balance()
 {
     // 02.11.26.MVS translated by Antonio Volta 2022.07.29
