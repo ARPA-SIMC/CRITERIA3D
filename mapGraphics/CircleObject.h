dc0075df19d9ce84b1501946fc79e33bc3af58f3
ftomei <ftomei@arpae.it>
2022-11-21 20:18:02 +0100

    update lineObject

--- mapGraphics/CircleObject.h
+++ mapGraphics/CircleObject.h
@@ -8,7 +8,10 @@ class MAPGRAPHICSSHARED_EXPORT CircleObject : public MapGraphicsObject
 {
     Q_OBJECT
 public:
-    explicit CircleObject(qreal radius,bool sizeIsZoomInvariant=true, QColor fillColor = QColor(0,0,0,0), MapGraphicsObject *parent = nullptr);
+    explicit CircleObject(qreal radius,
+                          bool sizeIsZoomInvariant=true,
+                          QColor fillColor = QColor(0,0,0,0),
+                          MapGraphicsObject *parent = nullptr);
     virtual ~CircleObject() override;
 
     //pure-virtual from MapGraphicsObject
