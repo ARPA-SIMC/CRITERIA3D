0f78e3311cc355bab5e1d71e5709c240a2475ef6
ftomei <ftomei@arpae.it>
2023-02-14 18:59:23 +0100

    soil depth

--- agrolib/crop/root.cpp
+++ agrolib/crop/root.cpp
@@ -447,7 +447,7 @@ namespace root
             return false;
         }
 
-        double computationSoilDepth = soilLayers[nrLayers-1].depth + soilLayers[nrLayers-1].thickness / 2;
+        double soilDepth = soilLayers[nrLayers-1].depth + soilLayers[nrLayers-1].thickness / 2;
 
         // Initialize
         unsigned int i, layer;
@@ -467,7 +467,7 @@ namespace root
             unsigned int nrAtoms;
             nrAtoms = root::getNrAtoms(soilLayers, myCrop->roots.rootDepthMin, &minimumThickness, atoms);
             numberOfTopUnrootedLayers = int(round(myCrop->roots.rootDepthMin / minimumThickness));
-            numberOfRootedLayers = int(ceil(MINVALUE(myCrop->roots.rootLength, computationSoilDepth) / minimumThickness));
+            numberOfRootedLayers = int(ceil(MINVALUE(myCrop->roots.rootLength, soilDepth) / minimumThickness));
             double* densityThinLayers =  new double[nrAtoms];
 
             for (i=0; i < nrAtoms; i++)
