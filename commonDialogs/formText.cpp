fae940537d6c67369b639f0d203da44f0bcce986
ftomei <ftomei@arpae.it>
2023-03-31 16:35:34 +0200

    clean formText

--- commonDialogs/formText.cpp
+++ commonDialogs/formText.cpp
@@ -3,41 +3,19 @@
 FormText::FormText(QString title)
 {
     this->setWindowTitle(title);
-    QVBoxLayout* mainLayout = new QVBoxLayout;
     this->resize(250, 100);
-
-    QHBoxLayout *layoutOk = new QHBoxLayout;
-    QHBoxLayout *textLayout = new QHBoxLayout;
-
-    textLayout->addWidget(&textEdit);
 
     QDialogButtonBox buttonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
 
-    connect(&buttonBox, &QDialogButtonBox::accepted, [=](){ done(QDialog::Accepted); });
-    connect(&buttonBox, &QDialogButtonBox::rejected, [=](){ done(QDialog::Rejected); });
+    connect(&buttonBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
+    connect(&buttonBox, &QDialogButtonBox::rejected, this, &QDialog::reject);
 
-    layoutOk->addWidget(&buttonBox);
-
-    mainLayout->addLayout(textLayout);
-    mainLayout->addLayout(layoutOk);
+    QVBoxLayout* mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(&textEdit);
+    mainLayout->addWidget(&buttonBox);
 
     setLayout(mainLayout);
     exec();
-}
-
-
-void FormText::done(int res)
-{
-    if (res == QDialog::Accepted) // ok
-    {
-        QDialog::done(QDialog::Accepted);
-        return;
-    }
-    else    // cancel, close or exc was pressed
-    {
-        QDialog::done(QDialog::Rejected);
-        return;
-    }
 }
 
 
