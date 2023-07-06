8a25d9c019f9369cfeff8ef3e24399652d5f280b
ftomei <ftomei@arpae.it>
2023-07-06 15:27:31 +0200

    text multicolor bold

--- CircleObject.cpp
+++ CircleObject.cpp
@@ -57,6 +57,21 @@ void CircleObject::paint(QPainter *painter, const QStyleOptionGraphicsItem *opti
                 QPen myPen;
                 myPen.setColor(_fillColor);
                 painter->setPen(myPen);
+                if (! painter->font().bold())
+                {
+                    QFont currentFont = painter->font();
+                    currentFont.setBold(true);
+                    painter->setFont(currentFont);
+                }
+            }
+            else
+            {
+                if (painter->font().bold())
+                {
+                    QFont currentFont = painter->font();
+                    currentFont.setBold(false);
+                    painter->setFont(currentFont);
+                }
             }
 
             painter->scale(1,-1);
