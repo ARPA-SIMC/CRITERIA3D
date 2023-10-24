1edd13cd56cb58e6ca1ba541a69230f81ebd2ac4
ftomei <ftomei@arpae.it>
2023-10-24 17:59:12 +0200

    add build

---
+++ deploy/CRITERIA3D_build_MacOS.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# specify your Qt directory
+QT_DIR=/Users/xxxxxx/Qt/5.x.x/clang_64/bin
+QMAKE=$QT_DIR/qmake
+QDEPLOY=$QT_DIR/macdeployqt
+
+# build mapGraphics
+cd ../mapGraphics
+$QMAKE MapGraphics.pro -spec macx-clang CONFIG+=release CONFIG+=force_debug_info CONFIG+=x86_64 CONFIG+=qtquickcompiler
+make -f Makefile clean
+make -f Makefile all
+
+export LD_LIBRARY_PATH=`pwd`/../mapGraphics/release/:$LD_LIBRARY_PATH
+
+cd -
+
+# build CRITERIA3D
+cd ../bin/Makeall_CRITERIA3D
+$QMAKE Makeall_CRITERIA3D.pro -spec macx-clang CONFIG+=release CONFIG+=force_debug_info CONFIG+=x86_64 CONFIG+=qtquickcompiler
+make -f Makefile clean
+make -f Makefile qmake_all
+make 
+
+cd -
+
+# create directory
+mkdir CRITERIA3D
+mkdir CRITERIA3D/bin
+cd CRITERIA3D/bin
+
+# deploy app
+cp -r ../bin/CRITERIA3D/release/CRITERIA3D CRITERIA3D
+$DEPLOY CRITERIA3D.app
+
+cd -
+
+# copy doc and img
+mkdir CRITERIA3D/DOC
+mkdir CRITERIA3D/DOC/img
+cp ../DOC/CRITERIA3D.pdf CRITERIA3D/DOC/CRITERIA3D.pdf
+cp ../DOC/img/saveButton.png CRITERIA3D/DOC/img
+cp ../DOC/img/updateButton.png CRITERIA3D/DOC/img
+cp ../DOC/img/textural_soil.png CRITERIA3D/DOC/img
+
+# copy ALL data
+mkdir CRITERIA1D/DATA
+cp -R ../DATA/* CRITERIA1D/DATA/
+
---
+++ deploy/README.md
@@ -0,0 +1,34 @@
+    
+##	Build CRITERIA3D
+ 
+ 
+###	Dependencies
+
+	Qt libraries: Qt 5.4 or following is needed (download also QtCharts).
+ 
+ 
+###	Notes for Windows
+ 
+	Compiler:
+	MSVC = Microsoft Visual C++ compiler
+	MinGW = MinGW C++ compiler
+ 
+	build:
+	- open Qt shell (MSVC or MinGW version)
+	- move to deploy directory (cd [local path]\CRITERIA3D\deploy)
+	- execute CRITERIA3D_Build_Win_XXXX.bat 
+	
+	Only for MSVC:
+	before build execution call vcvarsall.bat to complete environment setup (with x64 option for 64-bit compilers)
+	example: 
+	> cd C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
+	> vcvarsall.bat x64
+ 
+
+###	Notes for Linux
+
+	Compiler:gcc v.6 or later
+	Qt version 5.4 or later
+        
+	build:
+	- execute CRITERIA3D_build_Linux.sh
