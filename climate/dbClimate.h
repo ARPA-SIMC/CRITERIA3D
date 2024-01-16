6318597e7f70599a1151e62febadda14b78b5bae
Gabriele Antolini <gantolini@arpae.it>
2024-01-16 11:35:02 +0100

    clean climate

--- climate/dbClimate.h
+++ climate/dbClimate.h
@@ -12,9 +12,9 @@
     bool saveAnnualElab(QSqlDatabase db, QString *myError, QString id, float result, QString elab);
     bool saveGenericElab(QSqlDatabase db, QString *myError, QString id, float result, QString elab);
 
-    bool selectAllElab(QSqlDatabase db, QString *myError, QString table, QList<QString>* listElab);
+    bool getClimateFieldsFromTable(QSqlDatabase db, QString *myError, QString climateTable, QList<QString>* fieldList);
     bool selectVarElab(QSqlDatabase db, QString *myError, QString table, QString variable, QList<QString>* listElab);
-    bool showClimateTables(QSqlDatabase db, QString *myError, QList<QString>* climateTables);
+    bool getClimateTables(QSqlDatabase db, QString *myError, QList<QString>* climateTables);
 
     bool deleteElab(QSqlDatabase db, QString *myError, QString table, QString elab);
 
