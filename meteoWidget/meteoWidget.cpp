dddc2bbb43adceb75c0db173d84c30c13b211707
lauracosta <lauramail@iol.it>
2024-04-10 18:58:25 +0200

    var colorBar

--- meteoWidget/meteoWidget.cpp
+++ meteoWidget/meteoWidget.cpp
@@ -712,7 +712,7 @@ void Crit3DMeteoWidget::resetValues()
                 else
                 {
                     QColor newColor;
-                    if (lineColor.red() > 255/2)
+                    if (lineColor.red() >= 255/2)
                     {
                         newColor.setRed(lineColor.red() - (255-lineColor.red()) * (mp+1)/nMeteoPoints);
                     }
@@ -720,7 +720,7 @@ void Crit3DMeteoWidget::resetValues()
                     {
                         newColor.setRed(lineColor.red() + (255-lineColor.red()) * (mp+1)/nMeteoPoints);
                     }
-                    if (lineColor.green() > 255/2)
+                    if (lineColor.green() >= 255/2)
                     {
                         newColor.setGreen(lineColor.green() - (255-lineColor.green()) * (mp+1)/nMeteoPoints);
                     }
@@ -728,7 +728,7 @@ void Crit3DMeteoWidget::resetValues()
                     {
                         newColor.setGreen(lineColor.green() + (255-lineColor.green()) * (mp+1)/nMeteoPoints);
                     }
-                    if (lineColor.blue() > 255/2)
+                    if (lineColor.blue() >= 255/2)
                     {
                         newColor.setBlue(lineColor.blue() - (255-lineColor.blue()) * (mp+1)/nMeteoPoints);
                     }
@@ -773,9 +773,30 @@ void Crit3DMeteoWidget::resetValues()
                     else
                     {
                         QColor newColor;
-                        newColor.setRed(barColor.red() + (255-barColor.red()) * mp/nMeteoPoints);
-                        newColor.setGreen(barColor.green() + (255-barColor.green()) * mp/nMeteoPoints);
-                        newColor.setBlue(barColor.blue() + (255-barColor.blue()) * mp/nMeteoPoints);
+                        if (barColor.red() >= 255/2)
+                        {
+                            newColor.setRed(barColor.red() - (255-barColor.red()) * (mp+1)/nMeteoPoints);
+                        }
+                        else
+                        {
+                            newColor.setRed(barColor.red() + (255-barColor.red()) * (mp+1)/nMeteoPoints);
+                        }
+                        if (barColor.green() >= 255/2)
+                        {
+                            newColor.setGreen(barColor.green() - (255-barColor.green()) * (mp+1)/nMeteoPoints);
+                        }
+                        else
+                        {
+                            newColor.setGreen(barColor.green() + (255-barColor.green()) * (mp+1)/nMeteoPoints);
+                        }
+                        if (barColor.blue() >= 255/2)
+                        {
+                            newColor.setBlue(barColor.blue() - (255-barColor.blue()) * (mp+1)/nMeteoPoints);
+                        }
+                        else
+                        {
+                            newColor.setBlue(barColor.blue() + (255-barColor.blue()) * (mp+1)/nMeteoPoints);
+                        }
                         bar->setColor(newColor);
                         bar->setBorderColor(newColor);
                     }
