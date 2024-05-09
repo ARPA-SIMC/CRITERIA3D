01751f8211efd84a5bea209537d1f95ce0a77489
lauracosta <lauramail@iol.it>
2024-05-09 14:32:53 +0200

    include QMap

--- waterTable/importRegistry.cpp
+++ waterTable/importRegistry.cpp
@@ -1,1 +1,8 @@
- 
+#include "importRegistry.h"
+#include <QFile>
+
+bool loadCsvRegistry(QString csvRegistry, QString *errorStr)
+{
+
+    return true;
+}
--- waterTable/well.h
+++ waterTable/well.h
@@ -4,6 +4,7 @@
 #include <QString>
 #include <QList>
 #include <QDate>
+#include <QMap>
 
 class Well
 {
