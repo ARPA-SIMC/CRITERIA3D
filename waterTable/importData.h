5d3fca2f2f090bca68f326e2f4646904067ac9b1
Antonio Volta <avolta@ARPA.EMR.NET>
2024-05-30 15:21:36 +0200

    fix conflict

--- waterTable/importData.h
+++ waterTable/importData.h
@@ -8,8 +8,7 @@
 
 bool loadWaterTableLocationCsv(const QString &csvFileName, std::vector<Well> &wellList, QString &errorStr, int &wrongLines);
 bool loadWaterTableDepthCsv(const QString &csvFileName, std::vector<Well> &wellList, int waterTableMaximumDepth, QString &errorStr, int &wrongLines);
-bool loadCsvDepthsSingleWell(QString csvDepths, Well well, int waterTableMaximumDepth, QString &errorStr, int &wrongLines);
-
+bool loadCsvDepthsSingleWell(QString csvDepths, Well* well, int waterTableMaximumDepth, QDate climateObsFirstDate, QDate climateObsLastDate, QString &errorStr, int &wrongLines);
 
 #endif // IMPORTDATA_H
 
