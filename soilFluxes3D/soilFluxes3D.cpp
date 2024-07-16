e522c5e7db92d4566ebedfcd1e85e989ada69675
ftomei <ftomei@arpae.it>
2024-07-16 16:19:06 +0200

    update 3D

--- soilFluxes3D/soilFluxes3D.cpp
+++ soilFluxes3D/soilFluxes3D.cpp
@@ -658,7 +658,7 @@ int DLL_EXPORT __STDCALL setHydraulicProperties(int waterRetentionCurve,
 
  /*!
   * \brief getTotalWaterContent
-  * \return total water content [m^3]
+  * \return total water content [m3]
   */
  double DLL_EXPORT __STDCALL getTotalWaterContent()
  {
@@ -823,10 +823,15 @@ int DLL_EXPORT __STDCALL setHydraulicProperties(int waterRetentionCurve,
  void DLL_EXPORT __STDCALL initializeBalance()
 {
     InitializeBalanceWater();
+
     if (myStructure.computeHeat)
+    {
         initializeBalanceHeat();
+    }
     else
+    {
         balanceWholePeriod.heatMBR = 1.;
+    }
 }
 
 
