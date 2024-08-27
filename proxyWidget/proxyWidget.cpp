6a81cc1eabfe72438cdf8976b4930acb8b61d259
Caterina Toscano <caterina.t98@gmail.com>
2024-08-27 08:12:40 +0200

    fix bug

--- proxyWidget/proxyWidget.cpp
+++ proxyWidget/proxyWidget.cpp
@@ -472,56 +472,58 @@ void Crit3DProxyWidget::modelLRClicked(int toggled)
             {
                 float myY;
                 std::vector<std::vector<double>> parameters = interpolationSettings->getFittingParameters();
-                if (interpolationSettings->getProxy(proxyPos)->getFittingFunctionName() == piecewiseThree)
+                if (parameters.size() > proxyPos)
                 {
-                    std::vector <double> xVector;
-                    for (int m = xMin; m < xMax; m += 5)
-                        xVector.push_back(m);
-
-                    for (int p = 0; p < int(xVector.size()); p++)
+                    if (parameters[proxyPos].size() == 5)
                     {
-                        point.setX(xVector[p]);
-                        point.setY(lapseRatePiecewise_three(xVector[p], parameters[proxyPos]));
-                        point_vector.append(point);
+                        std::vector <double> xVector;
+                        for (int m = xMin; m < xMax; m += 5)
+                            xVector.push_back(m);
+
+                        for (int p = 0; p < int(xVector.size()); p++)
+                        {
+                            point.setX(xVector[p]);
+                            point.setY(lapseRatePiecewise_three(xVector[p], parameters[proxyPos]));
+                            point_vector.append(point);
+                        }
                     }
-                }
-                else if (interpolationSettings->getProxy(proxyPos)->getFittingFunctionName() == piecewiseTwo)
-                {
-                    float lapseRateH0 = parameters[proxyPos][0];
-                    float lapseRateT0 = parameters[proxyPos][1];
-                    float slope1 = parameters[proxyPos][2];
-                    float slope2 = parameters[proxyPos][3];
-
-                    if (xMin < lapseRateH0)
+                    else if (parameters[proxyPos].size() == 4)
                     {
-                        myY = lapseRateT0 + slope1 * (xMin - lapseRateH0);
-                        point.setX(xMin);
+                        float lapseRateH0 = parameters[proxyPos][0];
+                        float lapseRateT0 = parameters[proxyPos][1];
+                        float slope1 = parameters[proxyPos][2];
+                        float slope2 = parameters[proxyPos][3];
+
+                        if (xMin < lapseRateH0)
+                        {
+                            myY = lapseRateT0 + slope1 * (xMin - lapseRateH0);
+                            point.setX(xMin);
+                            point.setY(myY);
+                            point_vector.append(point);
+                        }
+
+                        point.setX(lapseRateH0);
+                        point.setY(lapseRateT0);
+                        point_vector.append(point);
+
+                        myY = lapseRateT0 + slope2 * (xMax - lapseRateH0);
+                        point.setX(xMax);
                         point.setY(myY);
                         point_vector.append(point);
                     }
-
-                    point.setX(lapseRateH0);
-                    point.setY(lapseRateT0);
-                    point_vector.append(point);
-
-                    myY = lapseRateT0 + slope2 * (xMax - lapseRateH0);
-                    point.setX(xMax);
-                    point.setY(myY);
-                    point_vector.append(point);
-                }
-                else if (interpolationSettings->getProxy(proxyPos)->getFittingFunctionName() == piecewiseThreeFree)
-                {
-                    std::vector <double> xVector;
-                    for (int m = xMin; m < xMax; m += 5)
-                        xVector.push_back(m);
-
-                    for (int p = 0; p < int(xVector.size()); p++)
+                    else if (parameters[proxyPos].size() == 6)
                     {
-                        point.setX(xVector[p]);
-                        point.setY(lapseRatePiecewise_three_free(xVector[p], parameters[proxyPos]));
-                        point_vector.append(point);
-                    }
+                        std::vector <double> xVector;
+                        for (int m = xMin; m < xMax; m += 5)
+                            xVector.push_back(m);
 
+                        for (int p = 0; p < int(xVector.size()); p++)
+                        {
+                            point.setX(xVector[p]);
+                            point.setY(lapseRatePiecewise_three_free(xVector[p], parameters[proxyPos]));
+                            point_vector.append(point);
+                        }
+                    }
                 }
             }
 
