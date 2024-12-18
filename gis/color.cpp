3346131dc2bd54dc07e89ba1da345938d1a1eeac
ftomei <ftomei@arpae.it>
2024-12-18 19:23:03 +0100

    update 3D

--- gis/color.cpp
+++ gis/color.cpp
@@ -322,7 +322,7 @@ bool setSurfaceWaterScale(Crit3DColorScale* myScale)
     myScale->keyColor[0] = Crit3DColor(255, 255, 255);      /*!< white */
     myScale->keyColor[1] = Crit3DColor(0, 255, 255);        /*!< cyan */
     myScale->keyColor[2] = Crit3DColor(0, 0, 255);          /*!< blue */
-    myScale->keyColor[3] = Crit3DColor(255, 0, 0);          /*!< red */
+    myScale->keyColor[3] = Crit3DColor(128, 0, 255);        /*!< violet */
 
     return(myScale->classify());
 }
