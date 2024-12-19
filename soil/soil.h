4611b39429af2acffc3b3153814228afd1baba37
ftomei <ftomei@arpae.it>
2024-12-19 16:26:11 +0100

    fix soil fraction

--- soil/soil.h
+++ soil/soil.h
@@ -140,8 +140,11 @@
 
             double fieldCapacity;               /*!<  [kPa]     */
             double wiltingPoint;                /*!<  [kPa]     */
+
+            double waterContentSAT;             /*!<  [m3 m-3]*/
             double waterContentFC;              /*!<  [m3 m-3]*/
             double waterContentWP;              /*!<  [m3 m-3]*/
+
             double PH;                          /*!<  [-]       */
             double CEC;                         /*!<  [meq/100g]*/
 
