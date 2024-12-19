5d2e0e17d139dcbe9dd4244cf270ac7f091d28c3
ftomei <ftomei@arpae.it>
2024-12-19 16:35:34 +0100

    update 1D

--- soil/soil.cpp
+++ soil/soil.cpp
@@ -152,6 +152,7 @@ namespace soil
 
         this->fieldCapacity = NODATA;
         this->wiltingPoint = NODATA;
+
         this->waterContentSAT = NODATA;
         this->waterContentFC = NODATA;
         this->waterContentWP = NODATA;
@@ -225,15 +226,15 @@ namespace soil
         horizonPtr = horizonPointer;
 
         // [mm]
-        SAT = horizonPtr->waterContentSAT * thickness * 1000;
-        FC = horizonPtr->waterContentFC * thickness * 1000;
-        WP = horizonPtr->waterContentWP * thickness * 1000;
+        SAT = horizonPtr->waterContentSAT * thickness * 1000.;
+        FC = horizonPtr->waterContentFC * thickness * 1000.;
+        WP = horizonPtr->waterContentWP * thickness * 1000.;
         critical = FC;
 
         // hygroscopic humidity
-        double hygroscopicHumidity = -2000;     // [kPa]
+        double hygroscopicHumidity = -2000;                     // [kPa]
         double volWaterContentHH = soil::thetaFromSignPsi(hygroscopicHumidity, *horizonPtr);    // [m3 m-3]
-        HH = volWaterContentHH * horizonPtr->getSoilFraction() * thickness * 1000;
+        HH = volWaterContentHH * horizonPtr->getSoilFraction() * thickness * 1000.;             // [mm]
 
         return true;
     }
