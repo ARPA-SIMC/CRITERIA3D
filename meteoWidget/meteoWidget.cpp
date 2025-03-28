594d07ab4857c6ee438fa5e25f5f573b5b99c8e5
giadasan <gsannino@arpae.it>
2025-03-28 14:17:11 +0100

    try

--- meteoWidget/meteoWidget.cpp
+++ meteoWidget/meteoWidget.cpp
@@ -3077,8 +3077,8 @@ void Crit3DMeteoWidget::on_actionAddStation()
         QString station = stationId+"_"+stationsName;
         allStations << station;
     }
-    DialogAddStation selectStation(allStations);
 
+    DialogAddStation dialogAddStation(allStations, _allMeteoPointsPointer, _meteoPoints);
 }
 
 void Crit3DMeteoWidget::on_actionInfoPoint()
