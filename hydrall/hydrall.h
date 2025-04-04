a0bb994a69563b6ac1e1d521825ecf92242a3526
avolta <avolta@arpae.it>
2025-04-04 10:06:51 +0200

    aggiunta funzione correctionMoisture

--- hydrall/hydrall.h
+++ hydrall/hydrall.h
@@ -313,6 +313,7 @@
         std::vector<double> understoreyTranspirationRate;
         double understoreyAssimilationRate;
 
+        double moistureCorrectionFactor(int index);
         double understoreyRespiration();
         void radiationAbsorption();
         void setSoilVariables(int iLayer, int currentNode, float checkFlag, int horizonIndex, double waterContent, double waterContentFC, double waterContentWP, int firstRootLayer, int lastRootLayer, double rootDensity, double clay, double sand, double thickness, double bulkDensity, double waterContentSat);
