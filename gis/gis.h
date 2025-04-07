cccda9e91062aa474e628dfd4b28091be57fb70b
ftomei <ftomei@arpae.it>
2025-04-07 19:37:22 +0200

    test subtree push from praga

--- gis/gis.h
+++ gis/gis.h
@@ -285,6 +285,8 @@
         bool extractBasin(const Crit3DRasterGrid& inputRaster, Crit3DRasterGrid& outputRaster, double xClosure, double yClosure);
         void cleanRasterEmptyFrame(const Crit3DRasterGrid& inputRaster, Crit3DRasterGrid& outputRaster);
     }
+    
+    // test subtree push
 
 
 #endif // GIS_H
