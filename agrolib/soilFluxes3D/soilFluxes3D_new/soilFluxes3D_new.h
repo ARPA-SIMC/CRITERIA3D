c7fc6876fd38ed4cd70646b173ac1e794eb490af
IvanDB <ivan.debiasi99@gmail.com>
2025-07-15 14:48:09 +0200

    Refactor v0.1.2.2

---
+++ agrolib/soilFluxes3D/soilFluxes3D_new/soilFluxes3D_new.h
@@ -0,0 +1,16 @@
+#ifndef SOILFLUXES3D_NEW_H
+#define SOILFLUXES3D_NEW_H
+
+#include "macro.h"
+#include "types_cpu.h"
+
+namespace soilFluxes3D::New
+{
+    nodesData_t nodeGrid;
+    simulationParameters_t simulationParameters;
+
+    SF3Derror_t setNode_new(uint64_t index, double x, double y, double z, double volume_or_area, bool isSurface, boundaryType_t boundaryType, double slope = 0, double boundaryArea = 0);
+
+    SF3Derror_t setNodeLink_new(uint64_t nodeIndex, uint64_t linkIndex, linkType_t direction, double interfaceArea);
+}
+#endif // SOILFLUXES3D_NEW_H
