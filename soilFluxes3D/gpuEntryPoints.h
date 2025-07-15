6a3e6ff6ee14f0b3ee0d88881a25998ecd4c4f8c
IvanDB <ivan.debiasi99@gmail.com>
2025-07-15 14:38:38 +0200

    Refactor v0.1.2

--- soilFluxes3D/gpuEntryPoints.h
+++
@@ -1,8 +0,0 @@
-#ifndef GPUENTRYPOINTS_H
-#define GPUENTRYPOINTS_H
-
-#include "../types.h"
-
-void runCUSPARSEiteration(TmatrixElement **A, double* b, double* x, double* &vecSol, uint64_t numNodes);
-
-#endif // GPUENTRYPOINTS_H
