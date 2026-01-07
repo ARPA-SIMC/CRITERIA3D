d874ea98b6b29f2a7a8c4045dc66ac0e114afb79
ftomei <ftomei@arpae.it>
2026-01-07 18:02:10 +0100

    fix infiltration

--- agrolib/soilFluxes3D/water.h
+++ agrolib/soilFluxes3D/water.h
@@ -33,7 +33,7 @@ namespace soilFluxes3D::v2::Water
     __cudaSpec bool computeLinkFluxes(double &matrixElement, SF3Duint_t &matrixIndex, SF3Duint_t nodeIndex, u8_t linkIndex, u8_t approxNum, double deltaT, double lateralVerticalRatio, linkType_t linkType, meanType_t meanType);
 
     __cudaSpec double runoff(SF3Duint_t rowIdx, SF3Duint_t colIdx, u8_t approxNum, double deltaT, double flowSide);
-    __cudaSpec double infiltration(SF3Duint_t surfNodeIdx, SF3Duint_t soilNodeIdx, double deltaT, double flowArea, meanType_t meanType);
+    __cudaSpec double infiltration(SF3Duint_t surfNodeIdx, SF3Duint_t soilNodeIdx, u8_t approxNum, double deltaT, double flowArea, meanType_t meanType);
     __cudaSpec double redistribution(SF3Duint_t rowIdx, SF3Duint_t colIdx, double lateralVerticalRatio, double flowArea, linkType_t linkType, meanType_t meanType);
 
     double JacobiWaterCPU(VectorCPU& vectorX, const MatrixCPU &matrixA, const VectorCPU& vectorB);
