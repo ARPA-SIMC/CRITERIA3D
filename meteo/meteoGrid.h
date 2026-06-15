da73b8a24dea5f24bdb40b0cbd016e46cad1b393
ftomei <ftomei@arpae.it>
2026-06-15 19:08:49 +0200

    update climate

--- meteo/meteoGrid.h
+++ meteo/meteoGrid.h
@@ -121,7 +121,10 @@
 
             bool findMeteoPointFromId(unsigned *row, unsigned *col, const std::string &code);
             bool existsMeteoPointFromId(const std::string& id);
-            bool getMeteoPointActiveId(int row, int col, std::string &id);
+
+            bool isMeteoPointActive(int row, int col) const;
+            bool getMeteoPointActiveId(int row, int col, std::string &id) const;
+
             bool getLatFromId(std::string id, double* lat);
             bool getLatLonFromId(std::string id, double* lat, double* lon);
             bool getXYZFromId(std::string id, double* x, double* y, double* z);
